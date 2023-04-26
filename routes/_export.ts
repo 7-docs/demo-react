e90379414c737b4e4c2ebf40b914ff2c87b865fa
Lars Kappert <lars@webpro.nl>
2023-04-26 17:01:31 +0200

    Restore _app

--- routes/_export.ts
+++ routes/_export.ts
@@ -1,10 +1,12 @@
 // Exports router modules for serverless env that doesn't support the dynamic import.
 // This module will be updated automaticlly in develoment mode, do NOT edit it manually.
 
-import * as $0 from "./index.tsx";
-import * as $1 from "./api/completion.ts";
+import * as $0 from './index.tsx';
+import * as $1 from './api/completion.ts';
+import * as $2 from './_app.tsx';
 
 export default {
-  "/": $0,
-  "/api/completion": $1,
+  '/': $0,
+  '/api/completion': $1,
+  '/_app': $2,
 };
